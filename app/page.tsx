e83cacc0e3ab600e58e6fd1018037b8d84c71b93
azhang45 <anz0063@gmail.com>
2024-12-04 23:40:06 -0500

    placeholder home page for demo day

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,29 @@ import { ItemGrid } from "@/components/ItemGrid";
 
 export default function Home() {
   return (
-    <main className="container mx-auto px-4 py-8">
-      <h1 className="text-3xl font-bold mb-6">Home Page Test</h1>
-    </main>
+    <div className="flex flex-col md:flex-row">
+      <main className="container mx-auto px-4 py-8">
+        <h1 className="text-3xl font-bold mb-6">Welcome to Scotty's Shop!</h1>
+        <div className="grid grid-cols-2 gap-4">
+          <div className="max-w-sm rounded overflow-hidden shadow-lg">
+            <div className="px-6 py-4">
+              <div className="font-bold text-xl mb-2">Marketplace</div>
+              <p className="text-gray-700 text-base">
+                Featured Marketplace item
+              </p>
+            </div>
+          </div>
+            
+          <div className="max-w-sm rounded overflow-hidden shadow-lg">
+            <div className="px-6 py-4">
+              <div className="font-bold text-xl mb-2">Commissions</div>
+              <p className="text-gray-700 text-base">
+                Featured commission
+              </p>
+            </div>
+          </div>
+        </div>
+      </main>
+    </div>
   );
 }
