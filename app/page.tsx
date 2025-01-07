40456d7a2f918fe4852d6b5c35d213ae72406dd9
azhang45 <anz0063@gmail.com>
2025-01-07 17:46:53 -0500

    random featured items generated on home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,19 @@
-import { ItemGrid } from "@/components/ItemGrid";
+"use client"
+
+import { api } from "@/convex/_generated/api";
+import { ITEM_TYPE, MPITEM_STATUS } from "@/convex/constants";
+import { useQuery } from "convex/react";
+import ItemCard from "@/components/ItemCard";
+
 
 export default function Home() {
+  const mpItem = useQuery(api.mpItems.search,{status:MPITEM_STATUS.AVAILABLE});
+  const commItem = useQuery(api.commItems.search,{isAvailable: true});
+
+  const randomMPIndex = mpItem && mpItem.length>0 ? Math.floor(Math.random() * mpItem.length) : 0;
+  const randomCommIndex = commItem && commItem.length>0 ? Math.floor(Math.random() * commItem.length) : 0;
+  
+
   return (
     <div className="flex flex-col md:flex-row">
       <main className="container mx-auto px-4 py-8">
@@ -8,19 +21,33 @@ export default function Home() {
         <div className="grid grid-cols-2 gap-4">
           <div className="max-w-sm rounded overflow-hidden shadow-lg">
             <div className="px-6 py-4">
-              <div className="font-bold text-xl mb-2">Marketplace</div>
+              <div className="font-bold text-xl mb-2">Featured Marketplace Item</div>
+              {mpItem && mpItem.length > 0 ? (
+                <ItemCard
+                  itemId={mpItem[randomMPIndex]._id}
+                  type={ITEM_TYPE.MARKETPLACE}
+                />
+              ) : (
               <p className="text-gray-700 text-base">
-                Featured Marketplace item
+                No marketplace items found
               </p>
+              )}
             </div>
           </div>
             
           <div className="max-w-sm rounded overflow-hidden shadow-lg">
             <div className="px-6 py-4">
-              <div className="font-bold text-xl mb-2">Commissions</div>
+              <div className="font-bold text-xl mb-2">Featured Commission</div>
+              {commItem && commItem.length > 0 ? (
+                <ItemCard
+                  itemId={commItem[randomCommIndex]._id}
+                  type={ITEM_TYPE.COMMISSION}
+                />
+              ) : (
               <p className="text-gray-700 text-base">
-                Featured commission
+                No commissions found
               </p>
+              )}
             </div>
           </div>
         </div>
